ece9c02c24330b5b61602e251e92bcf986c312e8
Daksh Srivastava <daksh_s@hcl.com>
2017-11-09 17:34:20 +0530

    Implemented MongoDB integration with mLab to persist values across restarts

--- NodeServices/server.js
+++ NodeServices/server.js
@@ -4,16 +4,14 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var mongoClient = require('mongodb').MongoClient;
+var express    				= require('express');        // call express
+var app        				= express();                 // define our app using express
+var bodyParser 				= require('body-parser');
+var mongoClient 			= require('mongodb').MongoClient;
+var ObjectID	 			= require('mongodb').ObjectID;
 
-//Declare variables to be used here
-//A boolean value that depicts the current state of the lamp
-var isLampSwitchedOn = false;
-//A boolean value that governs if Arduino is to continue it's operation or not
-var isArduinoUpdating = true;
+// Include other JS files to implement abstraction
+var databaseConfigration 	= require('./databaseConf');
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -28,10 +26,14 @@ var mongoDB;
 // Make connection to the Database
 // =============================================================================
 // Connect to the db
-mongoClient.connect("mongodb://localhost:27017/homeautomation", function (err, db) {
+mongoClient.connect("mongodb://" + databaseConfigration.username + ":" + databaseConfigration.password 
+	+ "@" + databaseConfigration.databaseUrl + ":" + databaseConfigration.port + "/" + databaseConfigration.name, function (err, db) {
    	//Catch exceptions
-   	if(err)
+   	if(err) {
+   		console.log(credentials.username + ":" + credentials.password);
    		throw err;
+   	} else
+   	console.log("Connection to mLab successful.");
 
      //Assign varaible db to mongoDB global variable | This will be used throughout the app to communicate to DB
      mongoDB = db;
@@ -54,7 +56,7 @@ router.get('/', function(req, res) {
 
 //Toggle swit
 //Query Params : 
-//node_id : 0,1,2...xxx | The ID of the node that is being toggled
+//nodeId : xxxxxxx | The ID of the node that is being toggled
 //status  : true, fase | The new value of the node
 router.get('/toggleSwitch', function(req, res) {
 	
@@ -69,17 +71,22 @@ router.get('/toggleSwitch', function(req, res) {
 
 		//create a query for DB
 		var dbQuery = {
-			node_id: queryParams.node_id
+			nodeId: ObjectID(queryParams.nodeId)
 		};
 
 		//Values to be updated
-		var dbNewValue = {
-			isNodeTurnedOn: queryParams.status
-		};
+		var dbNewValue = {$set:{isNodeTurnedOn: (queryParams.status == 'true')}};
 
 		collection.updateOne(dbQuery, dbNewValue, function(err, items) {
 
-			console.log(items);
+			//Throw error if update failed
+			if(err)
+				throw err;
+
+			//Console output
+     		console.log(new Date() + " : " + "Switch recorded in DB."
+     			+ " Old node status : " + !queryParams.status
+     			+ " New node status : " + queryParams.status);
 
      		//Log the output back to the user
      		res.json({ 
@@ -88,11 +95,6 @@ router.get('/toggleSwitch', function(req, res) {
      			isArduinoUpdating	: queryParams.status,
      			success				: true
      		});
-
-     		//Console output
-     		console.log(new Date() + " : " + "Switch recorded in DB."
-     			+ " Old node status : " + !queryParams.status
-     			+ " New node status : " + queryParams.status);
      	});
 	});
 });
@@ -101,34 +103,45 @@ router.get('/toggleSwitch', function(req, res) {
 
 //Get updated status of the lamp | This is the API end point which will be accessed by either the Arduino or
 //a client that will pass on the state to the Arduino
+//Query Params : 
+//nodeId : xxxxxxx | The ID of the node that is being toggled
 router.get('/getStatus', function(req, res) {
 
-// collection.find({"node_id":1}).toArray(function(err, items){
-//      		//Console output
-//      		console.log(new Date() + " : " + "Switch toggle requested."
-//      			+ " Old lamp status : " + isLampSwitchedOn
-//      			+ " New lamp status : " + !isLampSwitchedOn
-//      		});
-//      	});
+	//Extract the query params
+	var queryParams = req.query;
 
-	//Console output
-	console.log(new Date() + " : " + "Lamp status requested. Current Lamp status : " + isLampSwitchedOn);
+	//Query DB and get values for lamp switched on and whether arduino is updating or not
+	mongoDB.collection("status", function(err, collection) {
 
-	//A message to be displayed back to the user
-	var strMessage = '';
+		//create a query for DB
+		var dbQuery = {
+			nodeId: ObjectID(queryParams.nodeId)
+		};
 
-	if(isLampSwitchedOn)
-		strMessage = "The lamp is currently switched on.";
-	else
-		strMessage = "The lamp is currently switched off."
+		collection.findOne(dbQuery, function(err, items) {
 
+			//Throw error if update failed
+			if(err)
+				throw err;
+			else
+				console.log(new Date() + " : " + "Lamp status requested. Current Lamp status : " + JSON.stringify(items));
 
-	//Log the output back to the user
-	res.json({ 
-		message 				: strMessage,
-		isLampSwitchedOn		: isLampSwitchedOn,
-		isArduinoUpdating 		: isArduinoUpdating,
-		success					: true
+			//A message to be displayed back to the user
+			var strMessage = '';
+
+			if(items.isNodeTurnedOn)
+				strMessage = "The lamp is currently switched on.";
+			else
+				strMessage = "The lamp is currently switched off."
+
+     		//Log the output back to the user
+     		res.json({
+     			message				: 'The switch has been toggled',
+     			isLampSwitchedOn	: items.isNodeTurnedOn,
+     			isArduinoUpdating	: true,
+     			success				: true
+     		});
+     	});
 	});
 });
 
@@ -137,33 +150,33 @@ router.get('/getStatus', function(req, res) {
 //Turn off Arduino from requesting lamp status on Google Cloud
 //Query Params : 
 //arduinoStatus : 1 or 0 | 0 = off | 1 = on
-router.get('/arduino', function(req, res) {
+// router.get('/arduino', function(req, res) {
 
-	//Console output
-	console.log(new Date() + " : " + "Arduino Toggle requested."
-		+ " Old status : " + isArduinoUpdating
-		+ "| New Status : " + !isArduinoUpdating);
+// 	//Console output
+// 	console.log(new Date() + " : " + "Arduino Toggle requested."
+// 		+ " Old status : " + isArduinoUpdating
+// 		+ "| New Status : " + !isArduinoUpdating);
 
-	//Extract the query params
-	var queryParams = req.query;
-	var strMessage = '';
+// 	//Extract the query params
+// 	var queryParams = req.query;
+// 	var strMessage = '';
 
-	//Update the continue Service variable with the one passed by user
-	isArduinoUpdating = queryParams.isArduinoUpdating === 'true';
+// 	//Update the continue Service variable with the one passed by user
+// 	isArduinoUpdating = queryParams.isArduinoUpdating === 'true';
 
-	if(!isArduinoUpdating)
-		strMessage = "Service requests to Google Console have been turned off for Arduino."; 
-	else
-		strMessage = "Service requests to Google Console have been turned on for Arduino."; 
+// 	if(!isArduinoUpdating)
+// 		strMessage = "Service requests to Google Console have been turned off for Arduino."; 
+// 	else
+// 		strMessage = "Service requests to Google Console have been turned on for Arduino."; 
 
-	//Send response
-	res.json({
-		message				: strMessage,
-		isLampSwitchedOn	: isLampSwitchedOn,
-		isArduinoUpdating 	: isArduinoUpdating,
-		success				: true
-	});
-});
+// 	//Send response
+// 	res.json({
+// 		message				: strMessage,
+// 		isLampSwitchedOn	: isLampSwitchedOn,
+// 		isArduinoUpdating 	: isArduinoUpdating,
+// 		success				: true
+// 	});
+// });
 
 // ============================================================================= //
 
