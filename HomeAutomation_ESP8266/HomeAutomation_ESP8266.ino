b955c8ed1fac8ecda26247352e07aadc8f2b39be
Daksh Srivastava <dakshsrivastava@gmail.com>
2018-01-07 21:32:28 +0530

    Modified ESP8266 code support garden project | Can execute motor to water plans directly from ESP8266 - Arduino not required

--- HomeAutomation_ESP8266/HomeAutomation_ESP8266.ino
+++ HomeAutomation_ESP8266/HomeAutomation_ESP8266.ino
@@ -2,19 +2,20 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-//#define RELAY1 2
-
-const char* ssid = "<SSID>";
-const char* password = "<WiFi Password>";
-const String serverUrl = "<sever url>/api";
+const char* ssid = "ENTER SSID HERE";
+const char* password = "ENTER WiFi PASSWORD HERE";
+const String serverUrl = "SERVER URL HERE";
 const String getStatusAPI = "/getStatus";
+const String updateMoistureLevel = "/updateMoistureLevel";
+const String nodeId = "NODE ID HERE";
 
+boolean isUpdated = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  pinMode(0, INPUT);
   pinMode(2, OUTPUT);
-  digitalWrite(2, LOW);
   WiFi.begin(ssid, password);  //Connect to the WiFi network
   while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
     delay(1000);
@@ -25,34 +26,57 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverUrl + getStatusAPI + "?nodeId=" + nodeId);
-    int httpCode = http.GET();
 
-    if (httpCode > 0) {
-      String jsonString = http.getString();
-      Serial.println(jsonString);
-      // read the incoming byte:
-      char json[400];
-      jsonString.toCharArray(json, 400);
-      StaticJsonBuffer<200> jsonBuffer;
-      JsonObject& root = jsonBuffer.parseObject(json);
-      if (root.containsKey("isLampSwitchedOn"))
-        if (root["isLampSwitchedOn"]) {
-          pinMode(2, OUTPUT);
-          digitalWrite(2, HIGH);
-          Serial.println("Relay On command was executed");
-        } else {
-          pinMode(2, INPUT);
-          Serial.println("Relay off command was executed");
-        }
+  //Check the current reading from the soil moisture module. The threshold using the potentiameter is set at 61%.
+  //if the value decreased below this, the value from DO changes to LOW. (Low on resistance, high on moisture) Switch the relay
+  //ON
+  int intMoistureLevel = digitalRead(0);
+  if (intMoistureLevel == HIGH) {
+    //Log to console | Moisture is LOW, resitance is HIGH
+    Serial.println("Moisture level is LOW");
+    //Switch GPIO2 to output mode and turn on the relay. This will start the pump
+    pinMode(2, OUTPUT);
+    digitalWrite(2, HIGH);
+    //Call the loop quicky again so that we don't over water the plants
+    delay(300);
+
+    //If the WiFi is connected, update the value in the system as well
+    if (WiFi.status() == WL_CONNECTED && !isUpdated) {
+      HTTPClient http;
+      http.begin(serverUrl + updateMoistureLevel + "?nodeId=" + nodeId + "&" + "status=" + "LOW");
+      int httpCode = http.GET();
+
+      if (httpCode > 0) {
+        String jsonString = http.getString();
+        Serial.println(jsonString);
+      }
+      http.end();
+      isUpdated = true;
     }
-    http.end();
-  } 
-  //else
-    //Serial.println("WiFi Not connected");
-  delay(3000);
-}
 
+  } else {
+    //Log to console | Soil moisture level is High, resistance is low
+    Serial.println("Moisture level is HIGH");
+    //If the WiFi is connected, update the value in the system as well
+    if (WiFi.status() == WL_CONNECTED && isUpdated) {
+      HTTPClient http;
+      http.begin(serverUrl + updateMoistureLevel + "?nodeId=" + nodeId + "&" + "status=" + "HIGH");
+      int httpCode = http.GET();
+
+      if (httpCode > 0) {
+        String jsonString = http.getString();
+        Serial.println(jsonString);
+      }
+      http.end();
+      //Inverse of previous if condition | This is done so that each of the If conditions to update the server is 
+      //done only once
+      isUpdated = false;
+    }
+
+    //Turn off the relay | Switch off the pump just in case
+    pinMode(2, INPUT);
+    //Moisture level is within threshold limit, Chill | Check after 1 hour
+    //delay(3600000);
+    delay(1000);
+  }
+}
