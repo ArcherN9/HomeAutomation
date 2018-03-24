103d4d8be658cd75b62dbe0831cb849db4d05021
Daksh Srivastava <daksh_s@hcl.com>
2018-03-24 20:14:36 +0530

    Initial Commit | Branched NodeServices to a different branch nodejs to begin integrating CI

---
+++ NodeServices/misc-conf.js
@@ -0,0 +1,11 @@
+//File used to configure Whatever URIs or other configurations that can be modified
+
+var sunriseSunsetURI = {
+	'host'			: "https://api.sunrise-sunset.org/",
+	'param'			: "json",
+	'latitude'		: "12.896942",
+	'longitude'		: "80.205872"
+}
+
+//Export to module to make it available globally
+module.exports = sunriseSunsetURI;
