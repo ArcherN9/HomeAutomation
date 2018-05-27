5e088b2b1d4e8da40ce145f3eb4904e8b7ce3779
Daksh Srivastava <daksh_s@hcl.com>
2018-05-27 21:31:16 +0530

    Fixed issues with db conf

--- NodeServices/databaseConf.js
+++ NodeServices/databaseConf.js
@@ -3,11 +3,22 @@
 var configuration = {
 	firebase 	: "https://daksh-home-automation.firebaseio.com",
 	mongoDB  	: {
+
+		// mLab Database | Production
 		"databaseUrl"	: "ds251985.mlab.com",
 		"port"			: 51985,
+		
+		//Local DB
+		// "databaseUrl"	: "localhost",
+		// "port"			: 27017,
+
+
 		"name"			: "homeautomation",
 		"username"		: "travisCI",
 		"password"		: "homeautomation"
+		
+		// "username"		: "dakshsrivastava",
+		// "password"		: "$May@2018#"
 	}
 }
 
